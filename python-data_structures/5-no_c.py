882f47995179485623c956dd9e6247546a89c6b2
elmir <ismayilovelmir32@gmail.com>
2026-04-04 19:38:34 +0000

    Add no_c function

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+
 def no_c(my_string):
-    new = ""
+    new_string = ""
     for char in my_string:
-        if char!="c" or char!="C":
-            new+=char
-    return new
+        if char != 'c' and char != 'C':
+            new_string += char
+    return new_string
