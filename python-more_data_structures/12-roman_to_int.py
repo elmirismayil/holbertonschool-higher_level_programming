0e9ac9588538ea15c49663167526f01c947bfb6a
elmir <ismayilovelmir32@gmail.com>
2026-04-06 14:44:56 +0000

    12

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    rom = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+    for char in reversed(roman_string):
+        val = rom.get(char, 0)
+        if val < last:
+            num -= val
+        else:
+            num += val
+        last = val
+
+    return num
