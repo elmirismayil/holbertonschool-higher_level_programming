3935bace0566dbf6c751d4779dc28d95441eda27
elmir <ismayilovelmir32@gmail.com>
2026-04-06 14:37:43 +0000

    10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
     b = list(a_dictionary.keys())[0]
     for key in a_dictionary:
         if a_dictionary[key]>a_dictionary[b]:
