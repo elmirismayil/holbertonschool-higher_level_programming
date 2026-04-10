51ddf12a2b25786b98d1f45e8f98e045ed392e69
elmir <ismayilovelmir32@gmail.com>
2026-04-10 13:18:24 +0000

    3

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -2,8 +2,9 @@
 def safe_print_division(a, b):
     result = None
     try:
-        result = a/b
+        result = a / b
     except ZeroDivisionError:
         pass
-    print("{}".format(result))
+    finally:
+        print("Inside result: {}".format(result))
     return result
